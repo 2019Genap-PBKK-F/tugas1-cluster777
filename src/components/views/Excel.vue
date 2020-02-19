9e58266e572029bf13e0404e0ac3e5595da2dfbf
cluster777 <42055002+cluster777@users.noreply.github.com>
2020-02-19 08:59:39 +0700

    coba2 belum terbaca

--- src/components/views/Excel.vue
+++ src/components/views/Excel.vue
@@ -1,76 +1,118 @@
 <template lang="html">
   <div class="wrapper-jexcel">
-    
-    <input
-      type='button'
-      value='Add new row'
-      @click='jExcelObj.insertRow()'
-    /><br />
+     <input type="button" value="Add new row" @click="() => jExcelObj.insertRow()" />
+      <input type="button" value="Delete row" @click="() => jExcelObj.deleteRow()" />
+    <br />
     <div id='spreadsheet' ref='spreadsheet'></div>
   </div>
 </template>
-<?php
-
-$host = "localhost"; /* Host name */
-$user = "root"; /* User */
-$password = ""; /* Password */
-$dbname = "coba"; /* Database name */
-
-$con = mysqli_connect($host, $user, $password,$dbname);
-// Check connection
-if (!$con) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-?>
 <script>
 import jexcelStyle from 'jexcel/dist/jexcel.css' // eslint-disable-line no-unused-vars
 import jexcel from 'jexcel' // eslint-disable-line no-unused-vars
-
-export default {
-  name: 'jexcel',
-  data() {
-    return {
-      myCars: [
-[ '0511140000089', 'a', '11-07-2009', '', '2014', true ],
-[ '0511140000039', 'b', '08-05-2009', '', '2014', true ],
-[ '0511140000029', 'c', '17-02-09', '', '2014', true ],
-[ '0511140000019', 'd', '22-02-09', '', '2014', false ],
-[ '0511140000009', 'e', '03-01-09', '', '2014', true ],
-[ '0511140000119', 'f', '25-10-09', '', '2014', true ]
-      ]
-    }
-  },
-  computed: {
-    jExcelOptions() {
-      return {
-        data: this.myCars,
-        columns: [
-{ type: 'text', title: 'nrp', width: '120px' },
-{ type: 'text', title: 'nama', width: '120px' },
-{ type: 'calendar', title: 'tanggal lahir', width: '120px' },
-{ type: 'image', title: 'photo', width: '120px' },
-{ type: 'text', title: 'angkatan', width: '120px' },
-{ type: 'checkbox', title: 'aktif', width: '120px' }
-        ]
+import axios from 'axios'
+var changed = function(instance, cell, x, y, value) {
+  x = parseInt(x)
+  y = parseInt(y)
+  var datatemp = []
+  datatemp[0] = y + 1
+  axios.get('http://localhost:27017/mahasiswa/' + datatemp[0]).then((response) => {
+    Object.keys(response.data).map(function (key) {
+      if (key === 'nama') {
+        datatemp[2] = response.data['nama']
       }
+      if (key === 'nrp') {
+        datatemp[1] = response.data['nrp']
+      }
+      if (key === 'angkatan') {
+        datatemp[3] = response.data['angkatan']
+      }
+      if (key === 'jeniskelamin') {
+        datatemp[4] = response.data['jeniskelamin']
+      }
+      if (key === 'tgllahir') {
+        datatemp[5] = response.data['tgllahir']
+      }
+      if (key === 'photo') {
+        datatemp[6] = response.data['photo']
+      }
+      if (key === 'aktif') {
+        datatemp[7] = response.data['aktif']
+      }
+    })
+    datatemp[x] = value
+    axios({
+      method: 'put',
+      url: 'http://localhost:27017/mahasiswa/' + datatemp[0],
+      data: {
+        id: datatemp[0],
+        nrp: datatemp[1],
+        nama: datatemp[2],
+        angkatan: datatemp[3],
+        jeniskelamin: datatemp[4],
+        tgllahir: datatemp[5],
+        photo: datatemp[6],
+        aktif: datatemp[7]
+      }
+    }).then((response) => {
+      console.log(response.data)
+    })
+  })
+}
+var insertrow = function(instance) {
+  axios({
+    method: 'post',
+    url: 'http://localhost:27017/mahasiswa/',
+    data: {
     }
+  }).then((response) => {
+    console.log(response.data)
+  })
+}
+var deleterow = function(instance) {
+  axios({
+    method: 'delete',
+    url: 'http://localhost:27017/mahasiswa/',
+    data: {
+    }
+  }).then((response) => {
+    console.log(response.data)
+  })
+}
+var options = {
+  url: 'http://localhost:27017/mahasiswa',
+  onchange: changed,
+  oninsertrow: insertrow,
+  ondeleterow: deleterow,
+  allowToolbar: true,
+  columns: [
+    { type: 'hidden', title: 'id', width: '120px' },
+    { type: 'text', title: 'NRP', width: '120px' },
+    { type: 'text', title: 'Nama', width: '120px' },
+    { type: 'text', title: 'Angkatan', width: '120px' },
+    { type: 'dropdown', title: 'Jenis Kelamin', width: '250px', autocomplete: true, source: ['Laki-Laki', 'Perempuan'] },
+    { type: 'calendar', title: 'Tgl-Lahir', width: '250px' },
+    { type: 'image', title: 'Photo', width: '120px' },
+    { type: 'checkbox', title: 'Aktif', width: '80px' }
+    // { type: 'dropdown', title: 'Make', width: '250px', source: [ 'Alfa Romeo', 'Audi', 'Bmw' ] },
+    // { type: 'calendar', title: 'Available', width: '250px' },
+    // { type: 'image', title: 'Photo', width: '120px' },
+    // { type: 'checkbox', title: 'Stock', width: '80px' },
+    // { type: 'numeric', title: 'Price', width: '100px', mask: '$ #.##,00', decimal: ',' },
+    // { type: 'color', width: '100px', render: 'square' }
+  ]
+}
+export default {
+  name: 'App',
+  mounted: function () {
+    this.load()
   },
   methods: {
-    insertRowc() {
-      console.log(this)
-      // this.spreadsheet.insertRow()
+    load() {
+      let spreadsheet = jexcel(this.$el, options)
+      Object.assign(this, { spreadsheet })
     }
-  },
-  mounted: function() {
-    // console.log(this.jExcelOptions)
-    // console.log(this.$refs['spreadsheet'])
-    const jExcelObj = jexcel(this.$refs['spreadsheet'], this.jExcelOptions)
-    // Object.assign(this, jExcelObj) // pollutes component instance
-    Object.assign(this, { jExcelObj }) // tucks all methods under jExcelObj object in component instance
-    // console.log(this.jExcelObj)
   }
 }
-
 </script>
 
 <style lang='css' scoped>
